cb855cf064c83e8d13cffaf83482473e60c6c9f1
Aditya Galande <adityagalande14@gmail.com>
2021-06-02 00:56:15 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // var char = document.getElementById('#char');
 window.addEventListener('keyup', xyz);
-  function xyz(e) {
-    var xyz = `${e.key} ${e.keyCode}`;
-   document.getElementById('char').innerHTML = xyz;
-  // char.innerHTML = xyz;
-  // console.log(e);
+//   function xyz(e) {
+//     var xyz = `${e.key} ${e.keyCode}`;
+//    document.getElementById('char').innerHTML = xyz;
+//   // char.innerHTML = xyz;
+//   // console.log(e);
+// }
+const arr = [];
+const secretCode = "neha";
+
+function xyz(e) {
+  console.log(e.keyCode);
+  arr.push(e.key.toLowerCase());
+  arr.splice(-secretCode.length - 1, arr.length - secretCode.length);
+
+  if(arr.join('').includes(secretCode)) {
+    console.log("Neha Jethwani");
+    cornify_add();
+  }
+  console.log(arr);
+
 }
